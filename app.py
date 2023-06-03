a8627276058630bf0b99dbc8f0540ccedc69b68f
Emmanuel Koomson <devfeefe@gmail.com>
2023-06-03 19:51:16 +0000

    added documentation for 2 functions

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask
 from datetime import datetime
 app = Flask(__name__)
 
+'''
+Function to check if app is running
+'''
 @app.route('/')
 def home():
     return 'app is running'
 
+'''
+Function to check current date and version of app running
+'''
 @app.route('/date')
 def get_date():
     today = datetime.today().strftime("%A, %d %B %Y")
